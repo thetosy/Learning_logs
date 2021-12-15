a65d98677290edfb43425d18ad44581716163403
tosy <tosy@tosys-MacBook-Pro.local>
2021-12-15 06:58:12 -0700

    DEBUG set based on env

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -130,5 +130,12 @@ LOGIN_URL = 'users:login'
 
 # Heroku settings
 import django_heroku
+import os
 
 django_heroku.settings(locals())
+
+if os.environ.get('DEBUG') == 'TRUE':
+    DEBUG = True
+elif os.environ.get('DEBUG') == 'FALSE':
+    DEBUG = False
+
